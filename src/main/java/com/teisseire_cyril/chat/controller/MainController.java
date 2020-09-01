0efae840b988beda867a7568a23e50cbf0319c82
Cyril <cyril.teisseire@orange.fr>
2020-09-01 15:17:13 +0200

    V1.1

--- src/main/java/com/teisseire_cyril/chat/controller/MainController.java
+++ src/main/java/com/teisseire_cyril/chat/controller/MainController.java
@@ -1,2 +1,55 @@
-package com.teisseire_cyril.chat.controller;public class MainController {
+package com.teisseire_cyril.chat.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public String index(HttpServletRequest request, Model model) {
+        String username = (String) request.getSession().getAttribute("username");
+
+        // If your are not logged, redirect to login page
+        if (username == null || username.isEmpty()) {
+            return "redirect:/login";
+        }
+        model.addAttribute("username", username);
+
+        // If you are log you will be redirect on chat page
+        return "chat";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        // Login page
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String doLogin(HttpServletRequest request, @RequestParam(defaultValue = "") String username) {
+        username = username.trim();
+
+        // If you have submit empty name you will be redirect on login page
+        if (username.isEmpty()) {
+            return "login";
+        }
+        request.getSession().setAttribute("username", username);
+
+        // If your name is not empty you will be redirect on chat page
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession(true).invalidate();
+
+        // You will be redirect on login page
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/teisseire_cyril/chat/controller/WebSocketController.java
+++ src/main/java/com/teisseire_cyril/chat/controller/WebSocketController.java
@@ -1,2 +1,27 @@
-package com.teisseire_cyril.chat.controller;public class WebSocketController {
+package com.teisseire_cyril.chat.controller;
+
+import com.teisseire_cyril.chat.model.MessageChat;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/publicChatRoom")
+    public MessageChat sendMessage(@Payload MessageChat chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/publicChatRoom")
+    public MessageChat addUser(@Payload MessageChat chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
 }
